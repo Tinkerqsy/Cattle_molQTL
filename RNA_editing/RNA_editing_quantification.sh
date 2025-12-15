1b867dac0aa4e7b9c883fcbdf43ffba8b812d188
syqiu <137684845+Tinkerqsy@users.noreply.github.com>
2025-12-15 11:28:33 +0800

    Update RNA_editing_quantification.sh

--- RNA_editing/RNA_editing_quantification.sh
+++ RNA_editing/RNA_editing_quantification.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # RNA editing quantification using REDItools2
 # Input : sorted BAM files
-# Output: RNA editing phenotype BED file for QTL mapping
+# Output: per-sample RNA editing site tables (.edit.txt)
+# Notes : Only uniquely mapped reads (MAPQ >= 60) are used
 
 ############################
 # Parameters
@@ -10,29 +11,57 @@ BAM_DIR=/path/to/bam_files
 REF_FASTA=/path/to/reference.fa
 OUT_DIR=./rna_editing
 REDITools=/path/to/reditools.py
-THREADS=1
 
+MAPQ=60
+BASE_QUAL=30
+MAP_QUAL=20
+THREADS=4
+MAX_JOBS=10
+
+mkdir -p ${OUT_DIR}/filtered_bam
 mkdir -p ${OUT_DIR}/edit_sites
 
 ############################
-# Run REDItools2
+# Step 1: Run REDItools2
 ############################
+job_count=0
+
 for bam in ${BAM_DIR}/*.bam; do
-    sample=$(basename ${bam} .bam)
-    echo "[INFO] Processing ${sample}"
+    (
+        sample=$(basename ${bam} .bam)
+        uniq_bam=${OUT_DIR}/filtered_bam/${sample}.uniq.bam
+        out_txt=${OUT_DIR}/edit_sites/${sample}.edit.txt
 
-    samtools index -@ 2 ${bam}
+        echo "[INFO] Processing ${sample}"
 
-    python ${REDITools} \
-        -f ${bam} \
-        -r ${REF_FASTA} \
-        -o ${OUT_DIR}/edit_sites/${sample}.edit.txt \
-        -t ${THREADS} \
-        -S \
-        --min_coverage 10 \
-        --min_base_quality 30 \
-        --min_mapping_quality 40 \
-        --output_edited_sites
+        # Filter uniquely mapped reads
+        if [ ! -s "${uniq_bam}" ]; then
+            samtools view -q ${MAPQ} -b -h ${bam} > ${uniq_bam}
+            samtools index ${uniq_bam}
+        fi
+
+        # Run REDItools2
+        python ${REDITools} \
+            -f ${uniq_bam} \
+            -r ${REF_FASTA} \
+            -o ${out_txt} \
+            -S \
+            -C \
+            -bq ${BASE_QUAL} \
+            -q ${MAP_QUAL} \
+            -T ${THREADS} \
+            -mrl 50 \
+            -os 5
+
+        echo "[INFO] Finished ${sample}"
+    ) &
+
+    job_count=$((job_count + 1))
+    if (( job_count % MAX_JOBS == 0 )); then
+        wait
+        echo "[INFO] Finished a batch of ${MAX_JOBS} samples."
+    fi
 done
 
-
+wait
+echo "[INFO] All RNA editing quantification jobs finished."
