21f6d9b1b86edd3b5fe0804174dec3ba91121190
syqiu <137684845+Tinkerqsy@users.noreply.github.com>
2025-12-15 11:26:40 +0800

    Update RNA_editing_quantification.sh

--- RNA_editing/RNA_editing_quantification.sh
+++ RNA_editing/RNA_editing_quantification.sh
@@ -15,7 +15,7 @@ THREADS=1
 mkdir -p ${OUT_DIR}/edit_sites
 
 ############################
-# Step 1: Run REDItools2
+# Run REDItools2
 ############################
 for bam in ${BAM_DIR}/*.bam; do
     sample=$(basename ${bam} .bam)
@@ -35,56 +35,4 @@ for bam in ${BAM_DIR}/*.bam; do
         --output_edited_sites
 done
 
-############################
-# Step 2: Build editing matrix (A>G / T>C)
-############################
-Rscript - <<'EOF'
-library(data.table)
-library(dplyr)
 
-indir <- "rna_editing/edit_sites"
-files <- list.files(indir, pattern="\\.edit\\.txt$", full.names=TRUE)
-
-edit_list <- list()
-
-for (f in files) {
-  sample <- sub("\\.edit\\.txt$", "", basename(f))
-  dt <- fread(f, data.table=FALSE)
-
-  colnames(dt)[1:9] <- c("Region","Position","Reference","Strand",
-                         "Coverage","MeanQ","BaseCount",
-                         "AllSubs","Frequency")
-
-  dt <- dt %>%
-    filter(Coverage >= 10,
-           Frequency >= 0.1, Frequency < 1,
-           (Reference=="A" & substr(AllSubs,2,2)=="G" & Strand==1) |
-           (Reference=="T" & substr(AllSubs,2,2)=="C" & Strand==2)) %>%
-    mutate(chr = paste0("chr", Region),
-           start = Position - 1,
-           end = Position,
-           ID = paste0(chr,":",Position,":",Reference,">",substr(AllSubs,2,2))) %>%
-    select(chr,start,end,ID,Frequency)
-
-  colnames(dt)[5] <- sample
-  edit_list[[sample]] <- dt
-}
-
-merged <- Reduce(function(x,y)
-  full_join(x,y,by=c("chr","start","end","ID")), edit_list)
-
-merged[is.na(merged)] <- 0
-merged <- merged %>% arrange(chr,start)
-
-fwrite(merged,
-       file="rna_editing/editing_matrix.bed",
-       sep="\t", quote=FALSE)
-EOF
-
-############################
-# Step 3: Sort & index BED
-############################
-bgzip -c ${OUT_DIR}/editing_matrix.bed > ${OUT_DIR}/editing_matrix.bed.gz
-tabix -p bed ${OUT_DIR}/editing_matrix.bed.gz
-
-echo "[INFO] RNA editing quantification finished."
